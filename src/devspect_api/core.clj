b04b3fa087034814fffb282980e7553bf9e5563a
Cory Flanigan <seeflanigan@gmail.com>
2013-06-05 07:50:45 -0600

    Add pg support via korma

--- src/devspect_api/core.clj
+++ src/devspect_api/core.clj
@@ -1,9 +1,22 @@
 (ns devspect-api.core
   (:use compojure.core)
+  (:use ring.adapter.jetty)
+  (:use korma.db)
+
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
-            [clojure.data.xml :as xml])
-  (:use ring.adapter.jetty))
+            [clojure.java.jdbc :as sql]
+            [clojure.string :as str]
+            [clojure.data.xml :as xml]))
+
+(def pg {
+     :db "devspect-api"
+     :user "c"
+     :host "localhost"
+     :port "4567"
+     :delimiters ""})
+
+(defdb prod (postgres pg))
 
 (defroutes my-routes
   (GET "/" [] "this is compojure")
@@ -19,6 +32,8 @@
       #(= key (:tag %1))
       (tree-seq :content :content ds))))))
 
+;; (map (fn [attr fn-name] (defn 'fn-name [ds] (get-attribute attr ds)))
+;;   {:author 'author-name :description 'description})
 (defn author-name [ds] (get-attribute :author ds))
 (defn description [ds] (get-attribute :description ds))
 (defn occurred-at [ds] (get-attribute :occurred_at ds))
