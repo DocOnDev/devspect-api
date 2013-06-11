c25e38c3a6ea13d18679e14ffe44dc47ca7ddd23
Cory Flanigan <seeflanigan@gmail.com>
2013-06-11 14:33:05 -0600

    Initial load of pivotal-tracker webhook data into db

--- src/devspect_api/core.clj
+++ src/devspect_api/core.clj
@@ -4,21 +4,17 @@
 
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
-            [clojure.java.jdbc :as sql]
+            [clojure.java.jdbc :as jdbc]
+            [clojure.java.jdbc.sql :as sql]
             [clojure.string :as str]
             [clojure.data.xml :as xml]))
 
-(def db
-  {:subprotocol "postgresql"
-   :subname "//127.0.0.1:5432/devspect-api" })
 
-(defroutes my-routes
-  (GET "/" [] "this is compojure")
-  (route/not-found "fore oh fore"))
+(defn parse-tracker-xml [xml-string] (xml/parse (java.io.StringReader. xml-string)))
 
-(def app (handler/site my-routes))
-
-(defn parse-tracker-xml [xml] (xml/parse xml))
+(defn parse-dt [input]
+  (let [local-dt-fmt (java.text.SimpleDateFormat. "yyyy/MM/dd HH:mm:ss zzz")]
+    (java.sql.Timestamp. (.getTime (.parse local-dt-fmt input)))))
 
 (defn get-attribute [key ds]
   (first (:content
@@ -26,10 +22,31 @@
       #(= key (:tag %1))
       (tree-seq :content :content ds))))))
 
-;; (map (fn [attr fn-name] (defn 'fn-name [ds] (get-attribute attr ds)))
-;;   {:author 'author-name :description 'description})
 (defn author-name [ds] (get-attribute :author ds))
 (defn description [ds] (get-attribute :description ds))
-(defn occurred-at [ds] (get-attribute :occurred_at ds))
+(defn occurred-at [ds] (parse-dt (get-attribute :occurred_at ds)))
 (defn activity-type [ds] (get-attribute :event_type ds))
-(defn project-id [ds] (get-attribute :project_id ds))
+(defn project-id [ds] (Integer. (get-attribute :project_id ds)))
+
+(def db
+  {:subprotocol "postgresql"
+   :subname "//127.0.0.1:5432/devspect-api" })
+
+(defn handle-pivotal-post [xml]
+  (let [ds (parse-tracker-xml xml)]
+    (jdbc/insert! db :pivotal_tracker
+       {:author_name (author-name ds)
+        :description (description ds)
+        :occurred_at (occurred-at ds)
+        :activity_type (activity-type ds)
+        :project_id (project-id ds)
+       }))
+  "OK")
+
+(defroutes my-routes
+  (GET "/" [] "this is compojure")
+  (POST "/pivotal-tracker" {body :body} (handle-pivotal-post body))
+  (route/not-found "fore oh fore"))
+
+(def app (handler/site my-routes))
+
--- test/devspect_api/core_test.clj
+++ test/devspect_api/core_test.clj
@@ -2,9 +2,10 @@
   (:require [clojure.test :refer :all]
             [devspect-api.core :refer :all]))
 
+(def xml-string (slurp "pivotal.xml"))
+
 (deftest test-parse-tracker-xml
-  (def tracker-xml (java.io.StringReader. (slurp "pivotal.xml")))
-  (def tracker-ds (parse-tracker-xml tracker-xml))
+  (def tracker-ds (parse-tracker-xml xml-string))
 
   (testing "Extracts the author name"
     (is (= "James Kirk" (author-name tracker-ds))))
@@ -13,12 +14,24 @@
     (is (= "James Kirk accepted \"More power to shields\"" (description tracker-ds))))
 
   (testing "Extracts the date"
-    (is (= "2009/12/14 14:12:09 PST" (occurred-at tracker-ds))))
+    (is (= #inst "2009-12-14T22:12:09.000000000-00:00" (occurred-at tracker-ds))))
 
   (testing "Extracts the activity type"
     (is (= "story_update" (activity-type tracker-ds))))
 
   (testing "Extracts the project id"
-    (is (= "26" (project-id tracker-ds))))
+    (is (= 26 (project-id tracker-ds))))
 )
 
+(defn get-request [resource app & params]
+  (app {:request-method :get :uri resource :params (first params)}))
+
+(defn post-request [resource app body & params]
+  (app {:request-method :post :uri resource :params (first params) :body body}))
+
+(deftest test-routes
+  (is (= 200 (:status (get-request "/" app))))
+  (is (= "this is compojure" (:body (get-request "/" app))))
+  (is (= 200 (:status (post-request "/pivotal-tracker" app xml-string))))
+  (is (= "OK" (:body (post-request "/pivotal-tracker" app xml-string))))
+)
