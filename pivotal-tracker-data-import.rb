a489ad5d7e84f658119234e8d9884a61a0fad6da
Cory Flanigan <seeflanigan@gmail.com>
2013-06-12 16:37:18 -0600

    Import script for pivotal data

---
+++ pivotal-tracker-data-import.rb
@@ -0,0 +1,69 @@
+require 'bundler'
+
+Bundler.setup
+
+require 'pry'
+require 'pivotal-tracker'
+require 'sequel'
+
+DB = Sequel.connect('postgres://localhost:5432/devspect-api')
+stories  =       DB[:pivotal_tracker_stories]
+projects =       DB[:pivotal_tracker_projects]
+story_statuses = DB[:pivotal_tracker_story_statuses]
+story_states   = DB[:pivotal_tracker_story_states]
+
+PivotalTracker::Client.token = '8b2ba20d2a1b4d4309a4868d62f53e7a'
+PivotalTracker::Client.use_ssl = true
+
+project = PivotalTracker::Project.find(707539)
+
+# PivotalTracker::Project.all.each do |project|
+projects.insert(
+  id:                         project.id,
+  owner:                      project.account,
+  current_iteration_number:   project.current_iteration_number,
+  current_velocity:           project.current_velocity,
+  first_iteration_start_time: project.first_iteration_start_time,
+  initial_velocity:           project.initial_velocity,
+  iteration_length:           project.iteration_length,
+  labels:                     project.labels,
+  last_activity_at:           project.last_activity_at,
+  name:                       project.name,
+  point_scale:                "{#{project.point_scale}}",
+  velocity_scheme:            project.velocity_scheme,
+  week_start_day:             project.week_start_day
+)
+
+project.stories.all.each do |story|
+  class StoryStatus < Sequel::Model(:pivotal_tracker_story_statuses)
+  end
+
+  current_status_id = StoryStatus.find_or_create(description: story.current_state).id
+
+  stories.insert(
+    id:                story.id,
+    name:              story.name,
+    accepted_at:       story.accepted_at,
+    created_at:        story.created_at,
+    current_status_id: current_status_id,
+    deadline:          story.deadline,
+    description:       story.description,
+    estimate:          story.estimate,
+    integration_id:    story.integration_id,
+    jira_id:           story.jira_id,
+    jira_url:          story.jira_url,
+    labels:            story.labels,
+    other_id:          story.other_id,
+    owned_by:          story.owned_by,
+    project_id:        story.project_id,
+    requested_by:      story.requested_by,
+    story_type:        story.story_type,
+    url:               story.url
+  )
+
+  story_states.insert(
+    start_date: Time.now, # local?
+    status_id:  current_status_id,
+    story_id:   story.id
+  )
+end
--- schema.sql
+++ schema.sql
@@ -1,8 +1,59 @@
-CREATE TABLE pivotal_tracker (
+CREATE TABLE pivotal_tracker_activities (
   id SERIAL PRIMARY KEY,
   author_name CHARACTER VARYING,
   description CHARACTER VARYING,
   occurred_at TIMESTAMP WITH TIME ZONE,
   activity_type CHARACTER VARYING,
-  project_id INTEGER
+  project_id INTEGER,
+  story_id INTEGER
+);
+
+CREATE TABLE pivotal_tracker_story_statuses (
+  id SERIAL PRIMARY KEY,
+  description CHARACTER VARYING
+);
+
+CREATE TABLE pivotal_tracker_story_states (
+  id SERIAL PRIMARY KEY,
+  start_date TIMESTAMP WITH TIME ZONE,
+  end_date TIMESTAMP WITH TIME ZONE,
+  status_id INTEGER,
+  story_id INTEGER
+);
+
+CREATE TABLE pivotal_tracker_stories (
+  id SERIAL PRIMARY KEY,
+  name CHARACTER VARYING,
+  accepted_at TIMESTAMP WITH TIME ZONE,
+  created_at TIMESTAMP WITH TIME ZONE,
+  current_status_id INTEGER,
+  deadline INTEGER,
+  description CHARACTER VARYING,
+  estimate INTEGER,
+  integration_id INTEGER,
+  jira_id INTEGER,
+  jira_url CHARACTER VARYING,
+  labels CHARACTER VARYING,
+  other_id INTEGER,
+  owned_by CHARACTER VARYING,
+  project_id INTEGER,
+  requested_by CHARACTER VARYING,
+  story_type CHARACTER VARYING,
+  url CHARACTER VARYING
+);
+
+CREATE TABLE pivotal_tracker_projects (
+  id SERIAL PRIMARY KEY,
+  owner CHARACTER VARYING,
+  current_iteration_number INTEGER,
+  current_velocity INTEGER,
+  first_iteration_start_time TIMESTAMP WITH TIME ZONE,
+  initial_velocity INTEGER,
+  iteration_length INTEGER,
+  labels CHARACTER VARYING,
+  last_activity_at TIMESTAMP WITH TIME ZONE,
+  name CHARACTER VARYING,
+  point_scale INTEGER[],
+  velocity_scheme CHARACTER VARYING,
+  week_start_day CHARACTER VARYING
 );
