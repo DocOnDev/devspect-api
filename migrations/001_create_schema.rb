38538c4f3c5af0d0bdceda9478720a9c3ffde3bf
Cory Flanigan <seeflanigan@gmail.com>
2013-06-25 21:53:16 -0600

    Change point_scale datatype to string. Yolo.

--- migrations/001_create_schema.rb
+++ migrations/001_create_schema.rb
@@ -60,7 +60,7 @@ Sequel.migration do
       String :labels
       DateTime :last_activity_at
       String :name
-      column :point_scale, 'INTEGER[]'
+      String :point_scale
       String :velocity_scheme
       String :week_start_day
     end
