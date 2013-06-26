db65d6cb26b331520776384639ebe4856740b115
Cory Flanigan <seeflanigan@gmail.com>
2013-06-25 19:57:27 -0600

    Whitespace

--- pivotal_tracker_data_import.rb
+++ pivotal_tracker_data_import.rb
@@ -6,6 +6,7 @@ require 'pivotal-tracker'
 require 'sequel'
 
 DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost:5432/devspect-api')
+
 stories  =        DB[:pivotal_tracker_stories]
 projects =        DB[:pivotal_tracker_projects]
 story_statuses =  DB[:pivotal_tracker_story_statuses]
