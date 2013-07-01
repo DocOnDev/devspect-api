ffcfd768c9dba3468e978788ed55c69ae649f901
Solomon White & Cory Flanigan <seeflanigan+rubysolo@gmail.com>
2013-07-01 16:51:41 -0600

    Reorganize tests and factor out spec_helper

--- test.rb
+++ test.rb
@@ -1,8 +1,10 @@
+require_relative 'spec_helper'
+
 require './app'
-require 'minitest/autorun'
-require 'minitest/pride'
 require 'rack'
 require 'rack/test'
+
+Tracker = FakeModel
 
 set :environment, :test
 
@@ -19,89 +21,90 @@ class AppTest < MiniTest::Unit::TestCase
     assert_equal "hello sinatra", last_response.body
   end
 
-  def test_cumulative_flow_endpoint
-    get "/cfd"
-    assert last_response.ok?
+  # def test_cumulative_flow_endpoint
+  #   get "/cfd"
+  #   assert last_response.ok?
 
-    assert_equal CumulativeFlow.report.to_json, last_response.body
-  end
+  #   assert_equal CumulativeFlow.report.to_json, last_response.body
+  # end
 
   def test_post_pivotal_tracker_xml
     post "/pivotal-tracker", File.read("pivotal.xml")
     assert last_response.ok?
     assert_equal "OK", last_response.body
   end
+end
 
-  def test_import_activity
-    # this test sucks
-    xml = Nokogiri::XML(File.read("pivotal.xml"))
-    assert_equal(nil, import_activity(xml))
-  end
+  # def test_import_activity
+  #   # this test sucks
+  #   xml = Nokogiri::XML(File.read("pivotal.xml"))
+  #   assert_equal(nil, import_activity(xml))
+  # end
 
-  def test_parsing_id_and_status
-    xml = Nokogiri::XML(File.read("pivotal.xml"))
-    assert_equal [109, "accepted"], parse_id_and_status(xml)
-  end
+  # def test_parsing_id_and_status
+  #   xml = Nokogiri::XML(File.read("pivotal.xml"))
+  #   assert_equal [109, "accepted"], parse_id_and_status(xml)
+  # end
 
-  def test_close_history_predicate_returns_false_when_history_is_nil
-    refute close_history?(nil, 1)
-  end
+  # def test_close_history_predicate_returns_false_when_history_is_nil
+  #   refute close_history?(nil, 1)
+  # end
 
-  def test_close_history_predicate_returns_false_when_status_ids_match
-    history = StoryHistory.new(status_id: 1)
-    refute close_history?(history, 1)
-  end
+  # def test_close_history_predicate_returns_false_when_status_ids_match
+  #   history = StoryHistory.new(status_id: 1)
+  #   refute close_history?(history, 1)
+  # end
 
-  def test_close_history_predicate_returns_true_when_status_ids_do_not_match
-    history = StoryHistory.new(status_id: 1)
-    assert close_history?(history, 2)
-  end
+  # def test_close_history_predicate_returns_true_when_status_ids_do_not_match
+  #   history = StoryHistory.new(status_id: 1)
+  #   assert close_history?(history, 2)
+  # end
 
-  def test_create_history_attrs_makes_a_hash_of_history_attributes
-    actual = history_attrs_for(1, 2)
-    assert_equal 1, actual[:story_id]
-    assert_equal 2, actual[:status_id]
-  end
+  # def test_create_history_attrs_makes_a_hash_of_history_attributes
+  #   actual = history_attrs_for(1, 2)
+  #   assert_equal 1, actual[:story_id]
+  #   assert_equal 2, actual[:status_id]
+  # end
 
-  def test_create_history_predicate_returns_false_when_valid_history_exists
-    h = StoryHistory.new(history_attrs_for(1, 2)).save
-    refute create_history?(1, 2)
-    h.delete
-  end
+  # def test_create_history_predicate_returns_false_when_valid_history_exists
+  #   h = StoryHistory.new(history_attrs_for(1, 2)).save
+  #   refute create_history?(1, 2)
+  #   h.delete
+  # end
 
-  def test_create_history_predicate_returns_true_when_no_history_exists
-    attrs = { story_id: 1, status_id: 2 }
-    h = StoryHistory.find(attrs)
-    h && h.delete
-    assert create_history?(1, 2)
-  end
+  # def test_create_history_predicate_returns_true_when_no_history_exists
+  #   attrs = { story_id: 1, status_id: 2 }
+  #   h = StoryHistory.find(attrs)
+  #   h && h.delete
+  #   assert create_history?(1, 2)
+  # end
 
-  # model tests
-  # what an astronomical mess!
-  def test_cumulative_flow_report
-    # TODO 2013/06/21
-    # need to test support for multiple dates/keys
-    # add logic to import process to get accurate start/end dates
-    # for histories, e.g. when was it accepted as start_date...
-    counts = {
-      icebox:    7,
-      backlog:   14,
-      started:   2,
-      finished:  1,
-      delivered: 1,
-      accepted:  41,
-      rejected:  1
-    }.sort
+  # # model tests
+  # # what an astronomical mess!
+  # def test_cumulative_flow_report
+  #   # TODO 2013/06/21
+  #   # need to test support for multiple dates/keys
+  #   # add logic to import process to get accurate start/end dates
+  #   # for histories, e.g. when was it accepted as start_date...
+  #   counts = {
+  #     icebox:    7,
+  #     backlog:   14,
+  #     started:   2,
+  #     finished:  1,
+  #     delivered: 1,
+  #     accepted:  41,
+  #     rejected:  1
+  #   }.sort
 
-    expected = { "2013-06-20" => Hash[counts] }
+  #   expected = { "2013-06-20" => Hash[counts] }
 
-    assert_equal expected, CumulativeFlow.report
-  end
+  #   assert_equal expected, CumulativeFlow.report
+  # end
 
-  def test_cumulative_flow_to_hash
-    expected = { foxy: 3 }
-    assert_equal expected, CumulativeFlow.new(description: "foxy", count: 3).to_hash
-  end
+  # def test_cumulative_flow_to_hash
+  #   expected = { foxy: 3 }
+  #   assert_equal expected, CumulativeFlow.new(description: "foxy", count: 3).to_hash
+  # end
 
 
   # def test_closing_history_when_none_exists
--- tracker_test.rb
+++ tracker_test.rb
@@ -1,6 +1,4 @@
-require 'minitest/spec'
-require 'minitest/autorun'
-require 'minitest/pride'
+require_relative 'spec_helper'
 
 require 'time'
 require 'nokogiri'
@@ -10,34 +8,20 @@ describe Tracker do
   it 'creates a story' do
     tracker.handle_activity(xml_doc)
 
-    fake_story.messages.must_equal [:create_story]
-    fake_story.data.must_equal     [story_attrs]
+    fake_story.messages.must_equal     [:create_story]
+    fake_story.data.flatten.must_equal [story_attrs]
   end
 
   it 'creates a history record on story creation' do
     tracker.handle_activity(xml_doc)
 
-    fake_history.messages.must_equal [:create_history]
-    fake_history.data.must_equal     [history_attrs]
+    fake_history.messages.must_equal     [:create_history]
+    fake_history.data.flatten.must_equal [history_attrs]
   end
 
   def teardown
     @fake_story = nil
     @fake_history = nil
-  end
-
-  class FakeModel
-    attr_reader :messages, :data
-
-    def initialize
-      @messages = []
-      @data = []
-    end
-
-    def method_missing(method_id, attrs)
-      @messages << method_id
-      @data << attrs
-    end
   end
 
   def tracker
@@ -53,7 +37,7 @@ describe Tracker do
   end
 
   def xml_doc
-    doc = Nokogiri::XML(create_story_xml)
+    doc = Nokogiri::XML(Fixtures.create_story_xml)
   end
 
   def story_attrs
@@ -73,34 +57,6 @@ describe Tracker do
     { "start_date"    => DateTime.parse("2013/07/01 17:42:51 UTC"),
       "current_state" => "unscheduled",
       "story_id"      => 52651969,
-    }
-  end
-
-  def create_story_xml
-    %Q{
-<?xml version="1.0" encoding="UTF-8"?>
-<activity>
-  <id type="integer">381300435</id>
-  <version type="integer">585</version>
-  <event_type>story_create</event_type>
-  <occurred_at type="datetime">2013/07/01 17:42:51 UTC</occurred_at>
-  <author>Cory Flanigan</author>
-  <project_id type="integer">707539</project_id>
-  <description>Cory Flanigan added &quot;Test story to determine cause of webhook bug&quot;</description>
-  <stories type="array">
-    <story>
-      <id type="integer">52651969</id>
-      <url>http://www.pivotaltracker.com/services/v3/projects/707539/stories/52651969</url>
-      <name>Test story to determine cause of webhook bug</name>
-      <story_type>feature</story_type>
-      <description>Does this give us different xml?</description>
-      <estimate type="integer">2</estimate>
-      <current_state>unscheduled</current_state>
-      <owned_by>Cory Flanigan</owned_by>
-      <requested_by>Cory Flanigan</requested_by>
-    </story>
-  </stories>
-</activity>
     }
   end
 end
